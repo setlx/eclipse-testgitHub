cc773e4ba331d20259d1ea5f0d31a0fd870f647f
setlx <setlx@xx.com>
2020-12-19 00:55:53 +0800

    local edit

--- src/main/java/cn/lx/TestGit.java
+++ src/main/java/cn/lx/TestGit.java
@@ -3,7 +3,7 @@ package cn.lx;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("...TestGt");
-		System.out.println("...TestGt");
+		System.out.println("...local edit");
 		
 	}
 }
