602cb42f73b517d879c4ebea4e5f42c9fcb8045e
setlx <setlx@xx.com>
2021-01-04 15:50:43 +0800

    第一次提交

---
+++ src/main/java/cn/bw/smbms/App.java
@@ -0,0 +1,13 @@
+package cn.bw.smbms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/cn/bw/smbms/pojo/User.java
@@ -0,0 +1,6 @@
+package cn.bw.smbms.pojo;
+public class User{
+    public static void main(String[] args) {
+        System.out.println("Holler word");
+    }
+}
