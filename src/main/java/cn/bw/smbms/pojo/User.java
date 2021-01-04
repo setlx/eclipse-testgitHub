25fddac1e85f25b9fc858dc5ae241904f04374d6
setlx <setlx@xx.com>
2021-01-04 21:18:39 +0800

    smbms2再次修改

--- src/main/java/cn/bw/smbms/pojo/User.java
+++ src/main/java/cn/bw/smbms/pojo/User.java
@@ -3,5 +3,6 @@ public class User{
     public static void main(String[] args) {
         System.out.println("Holler word");
         System.out.println("smbms2修改");
+        System.out.println("smbms2再次修改");
     }
 }
